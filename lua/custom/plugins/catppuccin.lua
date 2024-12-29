4aec8ca86bf8a53f03ffc37509d928d9c345cd2e
Abdelrahman Bahaa <abdelrahmanb015@gmail.com>
2024-12-29 17:03:14 +0200

    update theme

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -2,15 +2,7 @@ return {
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000,
-  config = function()
-    require('catppuccin').setup {
-      color_overrides = {
-        mocha = {
-          base = '#000000',
-        },
-      },
-    }
-
+  init = function()
     vim.cmd.colorscheme 'catppuccin-mocha'
   end,
 }
